0f4a98968999f9958dcdffcb2085eeac17cb1653
Nicholas <crazedglitch@yahoo.com>
2023-03-22 16:27:51 -0500

    lab main deliverables complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form = document.getElementById("create-task-form");
+const newTaskText = document.getElementById("new-task-description");
+const list = document.getElementById("tasks");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const li = document.createElement('li');
+    list.appendChild(li);
+
+  const newToDoItem = document.createElement("span");
+    newToDoItem.textContent = newTaskText.value;
+    li.appendChild(newToDoItem); 
+})
